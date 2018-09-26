675e4796aed2f8afa1f39e360e26d99a441afc76
Burak Acar <nburakacar@gmail.com>
2018-09-26 21:05:33 +0300

    Update
    has been updated to retrieve the values of the equation from the user

--- Secant method.py
+++ Secant method.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 from math import fabs 
-def f(x):
-    return (x**2-2*x-3)
+def f(a,b,c,x):
+    return (a*x**2+b*x+c)
 
+print("Enter the values a, b, c of the equation to be ax2 + bx + c")
+a= int(input("Enter a:"))
+b= int(input("Enter b:"))
+c= int(input("Enter c:"))
 def error(x1,x2):
     return((x1-x2)/x1)
 
 def RootControl(x1,x2):
-    if(f(x1)==0 or f(x2)==0):
-        if(f(x1)==0):
+    if(f(a,b,c,x1)==0 or f(a,b,c,x2)==0):
+        if(f(a,b,c,x1)==0):
             print("Root of the equation :" , x1)
-        if(f(x2)==0):
+        if(f(a,b,c,x2)==0):
             print("Root of the equation :", x2)
         return 1
     else:
         return 0
-x1 = int(input("Enter first value"))
-x2 = int(input("Enter second value"))
-ErrorValue = error(x1,x2)
-RootValue = RootControl(x1,x2)
+if(b**2-4*a*c<0):
+    print("there is no linear root of the equation you entered")
+    
+else:
+    
+    x1 =int(input("Enter first value"))
+    x2 = int(input("Enter second value"))
+    ErrorValue = error(x1,x2)
+    RootValue = RootControl(x1,x2)
 
-while(fabs(ErrorValue) >0.002 and RootValue != 1):
-    x3 = x1 - (f(x1)*(x2-x1))/(f(x2)-f(x1))
-    if(f(x2)-f(x1)==0):
-        ErrorValue=0
-    else:
-        ErrorValue = error(x1,x2)
-    print("x2: ",x2,"x1: ",x1, "change:", x3,"Error Value= ",ErrorValue, RootValue)
-    x1,x2 = x2,x3
+    while(fabs(ErrorValue) >0.002 and RootValue != 1):
+        if(f(a,b,c,x2)-f(a,b,c,x1)==0):
+            ErrorValue=1
+            print("division by zero")
+            continue
+        else:
+            ErrorValue = error(x1,x2)
+        x3 = x1 - (f(a,b,c,x1)*(x2-x1))/(f(a,b,c,x2)-f(a,b,c,x1))
+    
+        print("x2: ",x2,"x1: ",x1, "change:", x3,"Error Value= ",ErrorValue, RootValue)
+        x1,x2 = x2,x3
 
         
     
